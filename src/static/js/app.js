2c383a4af7a45a0a1d410a525f25f594d228f60d
Aymen271 <aymen.chabchoub1@esprit.tn>
2022-05-18 12:23:24 +0200

    commiting

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -96,7 +96,7 @@ function AddItemForm({ onNewItem }) {
                     value={newItem}
                     onChange={e => setNewItem(e.target.value)}
                     type="text"
-                    placeholder="New Item"
+                    placeholder="New"
                     aria-describedby="basic-addon1"
                 />
                 <InputGroup.Append>
