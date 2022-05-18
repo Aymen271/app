de34ed684941f3d2ce76fcc39d1d6c0574b32802
Aymen271 <aymen.chabchoub1@esprit.tn>
2022-05-18 15:11:10 +0200

    s

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">No items yet! Add one pls!</p>
+                <p className="text-center">No items yet! Add one pls thx!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
